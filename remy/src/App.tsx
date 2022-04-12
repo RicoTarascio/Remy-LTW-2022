cb11a6e206dbabcc1e116c8ea733fa240dc9290a
Federico Tarascio <fe.tarascio@gmail.com>
2022-04-12 15:59:53 +0200

    Textfield, fonts

--- remy/src/App.tsx
+++ remy/src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Textfield from './components/input/textfield/textfield';
 
 const App = () => {
   return (
     <>
       <h1>Remy</h1>
+      <div className='textfield-holder'>
+        <Textfield size="big" type="email" changeCallback={(e: any) => { console.log(e) }} placeholder='Inserisci la tua email' label='Email' required icon='Profile' />
+      </div>
+
     </>
   );
 }
---
+++ remy/src/components/input/textfield/textfield.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Icon } from "../../../icons/types";
+import { InputType } from "./inputTypes";
+import "./textfield.css";
+const Textfield = (
+    { type, changeCallback, required, label, placeholder, icon, size }:
+        { type: InputType, changeCallback: Function, required: boolean, label: string | undefined, placeholder: string | undefined, icon: Icon, size: string }) => {
+
+    const [typeController, setTypeController] = useState(type);
+    return (
+        <div className="textfield-container">
+            <h6 className="textfield-label ui-text">
+                {label} {required ? <span className="asterisc">*</span> : ""}
+            </h6>
+            <div className="input-icon">
+                <input className={`textfield ${size}`} type={typeController} placeholder={placeholder} required={required} onChange={(e) => { changeCallback(e) }} />
+                {type == "password" ? <div className="hide-show-password" onClick={() => setTypeController(hideShowPassword(typeController))}><i className={`icon ${icon}`}></i></div> : <i className={`icon ${icon}`}></i>}
+
+            </div>
+        </div>
+    )
+}
+
+const hideShowPassword = (type: InputType): InputType => {
+    if (type == "password") return "text"
+    return "password";
+}
+
+export default Textfield;
