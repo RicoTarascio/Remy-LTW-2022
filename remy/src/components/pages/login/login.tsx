4a1b125e7a4cd508e4365ad0a7ebc26bb30f7f0e
Federico Tarascio <fe.tarascio@gmail.com>
2022-04-15 12:14:15 +0200

    Login

--- remy/src/components/pages/login/login.tsx
+++ remy/src/components/pages/login/login.tsx
@@ -3,27 +3,37 @@ import { useState } from "react";
 import { Icon } from "../../../icons/types";
 import { InputType } from "../../input/textfield/inputTypes";
 */
+import { useEffect, useState } from "react";
+import Checkbox from "../../input/checkbox/checkbox";
 import Textfield from "../../input/textfield/textfield";
 import "./login.css";
 
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [checkbox, setCheckbox] = useState(false);
+
     return (
-        <>
-            <h2>Login</h2>
-            <div className='textfield-holder'>
-                <Textfield size="big" type="email" changeCallback={(e: any) => { console.log(e) }} placeholder='Inserisci la tua email' label='Email' required icon='Profile' />
-            </div>
+        <div className="login-container">
+            <div className="top"><h1>Remy</h1></div>
+            <div className="artwork"></div>
+            <div className="form">
+                <h2 className="login-title">Login</h2>
+                <div className="inner-form-container">
+                    <Textfield size="big" type="email" changeCallback={(e: any) => { setEmail(e.target.value); }} placeholder='Inserisci la tua email' label='Email' required icon='Profile' />
 
-            <div className='textfield-holder'>
-                <Textfield size="big" type="password" changeCallback={(e: any) => { console.log(e) }} placeholder='Inserisci la tua password' label='Password' required icon='Show' />
-            </div>
+                    <Textfield size="big" type="password" changeCallback={(e: any) => { setPassword(e.target.value); }} placeholder='Inserisci la tua password' label='Password' required icon='Show' />
 
-            <div>
-                <input type='checkbox' name="ricordami" ></input>
-                Ricordami
+                    <Checkbox label="Ricordami" changeCallback={(e: any) => { setCheckbox(e.target.checked); }} required></Checkbox>
+
+                    <button onClick={() => { }}>Login</button>
+                </div>
+
             </div>
-        </>
+            <div className="bottom"></div>
+
+        </div>
 
     );
 }
