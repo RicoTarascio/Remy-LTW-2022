577a6b64b190b9b94946da490f955dadbfbe8199
tranquilli-188710 <tranquilli.1887160@studenti.uniroma1.it>
2022-04-15 16:55:52 +0200

    home

--- remy/src/components/pages/home/home.tsx
+++ remy/src/components/pages/home/home.tsx
@@ -1,5 +1,17 @@
 const Home = () => {
-    return <h1>Home</h1>
+    return (
+        <div className="home-container">
+            <h1>Home</h1>
+            <div className="text-container">
+                <div className="slogan-container">
+                    <h1>Make your pets eat the right way</h1>
+                </div>
+                <div className="subtitle-container">
+                    <h3>Remy è il dietologo di fiducia dei tuoi animali domestici</h3>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
