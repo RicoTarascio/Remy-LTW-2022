f809672eed6a89d41965e22833f06a0bc9872e4e
Federico Tarascio <fe.tarascio@gmail.com>
2022-04-25 11:07:27 +0200

    home styling fix

--- remy/src/components/pages/home/home.tsx
+++ remy/src/components/pages/home/home.tsx
@@ -3,32 +3,24 @@ import "./home.css";
 const Home = () => {
     return (
         <div className="home-container">
-            
             <div className="top">
                 <div className="logo-container">
                     <h1>Home</h1>
                 </div>
             </div>
-            
+
             <div className="text-container">
-                <div className="slogan-container-1">
-                    <h1>Make your pets</h1>
-                </div>
-                <div className="slogan-container-2">
-                    <h1>eat the right way</h1>
+                <div className="slogan-container">
+                    <h1 className="main">Make your pets <span><h1 className="main orange">eat the right way</h1></span></h1>
                 </div>
                 <div className="subtitle-container">
                     <h3>Remy è il dietologo di fiducia dei tuoi animali domestici</h3>
                 </div>
                 <div className="button-container">
                     <button>Start now</button>
-                    <span></span>
-                    <h6>It's free!</h6>
+                    <h6 className="free-label">It's free!</h6>
                 </div>
             </div>
-
-            
-            
         </div>
     );
 }
