d42396f777b558cf0b04dfeeacb0f82507c55926
Federico Tarascio <fe.tarascio@gmail.com>
2022-05-12 12:39:57 +0200

    findPets -> now you can query by petID

--- backend/src/core/db/queries/findPets.ts
+++ backend/src/core/db/queries/findPets.ts
@@ -1,9 +1,10 @@
 import prismaClient from "../prismaClient";
 
-const queryFindPets = async (userID: number, nutritionPlans?: Object) => {
+const queryFindPets = async (userID: number, nutritionPlans?: Object, petID?: number) => {
   return await prismaClient.pet.findMany({
     where: {
       userID: userID,
+      id: petID
     },
     include: nutritionPlans,
   });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import Router from "./core/routing/router";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import queryFindPets from "./core/db/queries/findPets";
 
 dotenv.config();
 const app = express();
@@ -21,6 +22,6 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(Router);
 
-app.listen(process.env.SERVER_PORT, () => {
+app.listen(process.env.SERVER_PORT, async () => {
   console.log(`Server running on port ${process.env.SERVER_PORT}`);
 });
